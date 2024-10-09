9a3c20cd09e76d1fa94b9b2333c76ac0c9ae5a54
Maksim Palenov <maksdendy43@gmail.com>
2024-10-10 00:40:23 +0300

    Hello World

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorld = applicationContext.getBean("helloworld", HelloWorld.class);
+
         System.out.println(bean.getMessage());
+
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        Cat anotherCat = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(helloWorld == bean);
+        System.out.println(cat == anotherCat);
     }
 }
